70177f589020119bc149bbaaa13fc548f58a303e
Guilherme Oliveira <guilhermeoliveira36023@gmail.com>
2023-09-26 03:11:41 -0300

    http request tested and working

--- Main_form.cs
+++ Main_form.cs
@@ -2,17 +2,25 @@ namespace DR_Collector
 {
     public partial class Main_form : Form
     {
+
+        string data = string.Empty;
+
         public Main_form()
         {
             InitializeComponent();
         }
 
-        private async void btn_Get_Click(object sender, EventArgs e)
+        public async void btn_Get_Click(object sender, EventArgs e)
         {
             var httpClient = new HttpClient();
 
-            //var response  = await httpClient.GetAsync()
+            var response = await httpClient.GetAsync("http://172.16.0.2:55402/api/sqs/results?dateFrom=2023-07-29T04:20:00&dateTo=2023-07-29T04:30:02&station=Station-11&uniqueId=11-99.Work%20Finish");
 
+            string data = await response.Content.ReadAsStringAsync();
+
+            data = data.Trim(); 
         }
+
+
     }
 }
